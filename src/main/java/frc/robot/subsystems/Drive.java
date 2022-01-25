503700c9f86ce4a9781ca560a574adab4a360b40
fluffhead921 <routhmaya@gmail.com>
2022-01-25 17:00:47 -0600

    fixed formatting of tabs
    this doesn't nececessarily fit in this branch but it needed done ASAP
    
    --Maya
    (beginning new standard of signing commits since it would be annoying to constantly switch accounts. Previous commits have been Braden and Gabe)

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -11,31 +11,31 @@ import com.ctre.phoenix.motorcontrol.can.*;
 import frc.robot.Constants;
 
 public class Drive extends SubsystemBase {
-  /** Creates a new drivetrain subsystem. */
-  public Drive() {
-      /* Create all our motors */
-      WPI_TalonSRX frontLeft = new WPI_TalonSRX(Constants.ID_TALON_FRONT_LEFT);
-      WPI_TalonSRX frontRight = new WPI_TalonSRX(Constants.ID_TALON_FRONT_RIGHT);
-      WPI_TalonSRX rearLeft = new WPI_TalonSRX(Constants.ID_TALON_REAR_LEFT);
-      WPI_TalonSRX rearRight = new WPI_TalonSRX(Constants.ID_TALON_REAR_RIGHT);
+    /** Creates a new drivetrain subsystem. */
+    public Drive() {
+        /* Create all our motors */
+        WPI_TalonSRX frontLeft = new WPI_TalonSRX(Constants.ID_TALON_FRONT_LEFT);
+        WPI_TalonSRX frontRight = new WPI_TalonSRX(Constants.ID_TALON_FRONT_RIGHT);
+        WPI_TalonSRX rearLeft = new WPI_TalonSRX(Constants.ID_TALON_REAR_LEFT);
+        WPI_TalonSRX rearRight = new WPI_TalonSRX(Constants.ID_TALON_REAR_RIGHT);
 
-      /* Invert the motor's driving direction */
-      frontLeft.setInverted(false);
-      frontRight.setInverted(false);
-      rearLeft.setInverted(false);
-      rearRight.setInverted(false);
-      
-      /* I believe this takes the motors and makes a list out of it called drivetrain. */ 
-      MecanumDrive drivetrain = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
+        /* Invert the motor's driving direction */
+        frontLeft.setInverted(false);
+        frontRight.setInverted(false);
+        rearLeft.setInverted(false);
+        rearRight.setInverted(false);
+        
+        /* I believe this takes the motors and makes a list out of it called drivetrain. */ 
+        MecanumDrive drivetrain = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
     }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
+    @Override
+    public void simulationPeriodic() {
+        // This method will be called once per scheduler run during simulation
+    }
 }
