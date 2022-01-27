2e0446fad2132417e740b7ff6e139ab92dab41e8
notaroboticsenthusiast <91231142+notaroboticsenthusiast@users.noreply.github.com>
2022-01-27 17:56:40 -0600

    fix javadoc comments
    --Gabe

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -12,7 +12,7 @@ import frc.robot.Constants;
 
 public class Drive extends SubsystemBase {
 
-    /** Variables */
+    /* Variables */
     WPI_TalonSRX frontLeft;
     WPI_TalonSRX rearLeft;
     WPI_TalonSRX frontRight;
@@ -22,13 +22,13 @@ public class Drive extends SubsystemBase {
 
     /** Creates a new drivetrain subsystem. */
     public Drive() {
-        /** Initializes drive motor controllers */
+        /* Initializes drive motor controllers */
         frontLeft = new WPI_TalonSRX(Constants.ID_TALON_FRONT_LEFT);
         rearLeft = new WPI_TalonSRX(Constants.ID_TALON_REAR_LEFT);
         frontRight = new WPI_TalonSRX(Constants.ID_TALON_FRONT_RIGHT);
         rearRight = new WPI_TalonSRX(Constants.ID_TALON_REAR_RIGHT);
 
-        /** Sets default drive directions */
+        /* Sets default drive directions */
         frontLeft.setInverted(false);
         rearLeft.setInverted(false);
         frontRight.setInverted(false);
@@ -45,12 +45,12 @@ public class Drive extends SubsystemBase {
          * components (similar to Constants).
          */
 
-        /** Initializes a mecanum drivetrain */ 
+        /* Initializes a mecanum drivetrain */ 
         drivetrain = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
     }
 
     public void move(double x, double y, double z,) {
-        /** Sets the default drive mode to Cartesian */
+        /* Sets the default drive mode to Cartesian */
         drivetrain.driveCartesian(x, y, z);
     }
 
