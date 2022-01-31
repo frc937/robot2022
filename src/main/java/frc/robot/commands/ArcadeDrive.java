cfac0a778d932a6d834c88da9026e389e9d8d8e7
fluffhead921 <routhmaya@gmail.com>
2022-01-31 17:18:08 -0600

    Created ArcadeDrive class, needs renaming
    The class name needs to be renamed to what drive we are actually using.
    
    --Braden

---
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -0,0 +1,9 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class ArcadeDrive extends CommandBase {
+    
+    /* Rename it from ArcadeDrive to what drive we are actually using. */
+
+}
