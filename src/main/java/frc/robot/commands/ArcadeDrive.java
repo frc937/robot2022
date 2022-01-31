8d01eef152d8574e33895c352212eb081c7a4459
fluffhead921 <routhmaya@gmail.com>
2022-01-31 17:23:18 -0600

    Renamed ArcadeDrive to CartesianDrive
    --Braden

--- src/main/java/frc/robot/commands/ArcadeDrive.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.commands;
-
-import edu.wpi.first.wpilibj2.command.CommandBase;
-
-public class ArcadeDrive extends CommandBase {
-    
-    /* Rename it from ArcadeDrive to what drive we are actually using. */
-
-}
