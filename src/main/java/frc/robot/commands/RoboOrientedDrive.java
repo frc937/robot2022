58927ffc3d657110cdc89f7e12239b03e8387be6
fluffhead921 <routhmaya@gmail.com>
2022-02-01 17:09:21 -0600

    Renamed CartesianDrive to RoboOrientedDrive
    --Braden

--- src/main/java/frc/robot/commands/RoboOrientedDrive.java
+++ src/main/java/frc/robot/commands/RoboOrientedDrive.java
@@ -2,7 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class CartesianDrive extends CommandBase {
+public class RoboOrientedDrive extends CommandBase {
     
     
 
