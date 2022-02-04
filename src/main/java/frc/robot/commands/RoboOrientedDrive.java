eeafd48f435a8f10d6d8065d07b5736dc0f66f78
fluffhead921 <routhmaya@gmail.com>
2022-02-04 17:29:24 -0600

    Initialize puts "robot" perspective in smartdash
    --Maya

--- src/main/java/frc/robot/commands/RoboOrientedDrive.java
+++ src/main/java/frc/robot/commands/RoboOrientedDrive.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.Drive;
@@ -16,6 +17,11 @@ public class RoboOrientedDrive extends CommandBase {
         drivetrain = driveSubsystem;
         addRequirements(driveSubsystem);
 
+    }
+
+    @Override
+    public void initialize() {
+        SmartDashboard.putString("Drive Perspective", "Robot");
     }
 
     @Override
