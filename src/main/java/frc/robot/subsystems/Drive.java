539405c95454416f2363461f1e32c27fc9c60394
fluffhead921 <routhmaya@gmail.com>
2022-02-04 18:17:47 -0600

    removed unused inputs
    --Maya

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -6,12 +6,10 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.*;
 import com.kauailabs.navx.frc.AHRS;
 
 import frc.robot.Constants;
-import frc.robot.Robot;
 
 public class Drive extends SubsystemBase {
 
