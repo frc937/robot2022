6f48feec7d3b2aa7e050709a5cec1345888258a7
NaCl10 <nacl10gaming@gmail.com>
2022-02-05 14:32:40 -0600

    use WPILib PID controller
    we have to because our encoders plug directly into the Rio, rather than into the SPARK MAXs

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -7,7 +7,6 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
@@ -33,11 +32,8 @@ public class Shooter extends SubsystemBase {
         flywheelEncoder = new Encoder(Constants.DIO_PIN_FLYWHEEL_ENCODER_0, Constants.DIO_PIN_FLYWHEEL_ENCODER_1);
         controlWheelEncoder= new Encoder(Constants.DIO_PIN_CONTROL_WHEEL_ENCODER_0, Constants.DIO_PIN_CONTROL_WHEEL_ENCODER_1);
 
-        flywheelPIDController = flywheel.getPIDController();
-        controlWheelPIDController = controlWheel.getPIDController();
-
-        flywheelPIDController.setFeedbackDevice(flywheelEncoder);
-
+        flywheelPIDController = new PIDController(Constants.kFLYWHEEL_P, Constants.kFLYWHEEL_I, Constants.kFLYWHEEL_D);
+        controlWheelPIDController = new PIDController(Constants.kCONTROL_WHEEL_P, Constants.kCONTROL_WHEEL_I, Constants.kCONTROL_WHEEL_D);
     }
 
     
