e8ce535a02ecc9727f49300991964a51fa95fd45
NaCl10 <nacl10gaming@gmail.com>
2022-02-05 14:47:32 -0600

    use  PIDSubsystem
    I'm learning, okay?

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,7 +4,7 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 import frc.robot.Constants;
 
 import com.revrobotics.CANSparkMax;
@@ -13,27 +13,23 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.Encoder;
 
-public class Shooter extends SubsystemBase {
+public class Shooter extends PIDSubsystem {
 
     private CANSparkMax flywheel;
     private CANSparkMax controlWheel;
 
     private Encoder flywheelEncoder;
     private Encoder controlWheelEncoder;
-    
-    private PIDController flywheelPIDController;
-    private PIDController controlWheelPIDController;
 
     /** Creates a new Shooter. */
     public Shooter() {
+        super(new PIDController(Constants.kFLYWHEEL_P, Constants.kFLYWHEEL_I, Constants.kFLYWHEEL_D));
+
         flywheel = new CANSparkMax(Constants.ID_SPARKMAX_FLYWHEEL, MotorType.kBrushed);
         controlWheel = new CANSparkMax(Constants.ID_SPARKMAX_CONTROL_WHEEL, MotorType.kBrushed);
 
         flywheelEncoder = new Encoder(Constants.DIO_PIN_FLYWHEEL_ENCODER_0, Constants.DIO_PIN_FLYWHEEL_ENCODER_1);
         controlWheelEncoder= new Encoder(Constants.DIO_PIN_CONTROL_WHEEL_ENCODER_0, Constants.DIO_PIN_CONTROL_WHEEL_ENCODER_1);
-
-        flywheelPIDController = new PIDController(Constants.kFLYWHEEL_P, Constants.kFLYWHEEL_I, Constants.kFLYWHEEL_D);
-        controlWheelPIDController = new PIDController(Constants.kCONTROL_WHEEL_P, Constants.kCONTROL_WHEEL_I, Constants.kCONTROL_WHEEL_D);
     }
 
     
