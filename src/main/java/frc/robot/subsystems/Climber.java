75018133c0a752cbd44515f77cb6aeb4e59e2ff2
fluffhead921 <routhmaya@gmail.com>
2022-02-08 17:46:17 -0600

    Added (hopefully) helpful comments in climber.java
    --Braden

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -7,25 +7,23 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Climber {
 
+    /* Variables */
     private CANSparkMax winch;
     
+    /* Creates a new Climber Subsystem */
     public Climber() {
 
+        /* Initializes winch motor */
         winch = new CANSparkMax(Constants.ID_SPARKMAX_WINCH, MotorType.kBrushed);
 
     }
 
     public void runClimber() {
-        
         winch.set(Constants.WINCH_SPEED);
-
     }
 
     public void stopClimber() {
-
         winch.set(0);
-        
     }
-
 
 }
