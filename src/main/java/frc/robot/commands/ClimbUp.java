22109f97c8a175d3e62c3102e0015ccec5f63e1c
fluffhead921 <routhmaya@gmail.com>
2022-02-08 18:19:06 -0600

    Renamed Climb to ClimbUp, Added 2 classes for Climb
    Added the ClimbDown class and ClimberReset
    
    --Braden

---
+++ src/main/java/frc/robot/commands/ClimbUp.java
@@ -0,0 +1,11 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.Climber;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class ClimbUp extends CommandBase {
+
+    
+    
+}
