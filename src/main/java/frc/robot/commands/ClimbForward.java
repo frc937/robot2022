49fe61e18902aad387b400138a2428be1034b831
fluffhead921 <routhmaya@gmail.com>
2022-02-11 17:19:01 -0600

    Copied code over to ClimbForward from RunFlywheel command
    --Braden

--- src/main/java/frc/robot/commands/ClimbForward.java
+++ src/main/java/frc/robot/commands/ClimbForward.java
@@ -1,16 +1,32 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.Climber;
+import frc.robot.Constants;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
+import edu.wpi.first.math.controller.PIDController;
 
-public class ClimbForward extends CommandBase {
+
+public class ClimbForward extends PIDCommand {
 
     /* Variables */
-    private Climber climberInABox;
+    // this variable maybe shouldn't be static, but I don't know enough java to fix it when it's private
+    static Climber climberInABox;
 
     /* Initializes the ClimbUp method */
     public ClimbForward(Climber subsystemClimb) {
+        super(
+            // The controller that the command will use
+            new PIDController(Constants.kWINCH_P, Constants.kWINCH_I, Constants.kWINCH_D),
+            // This should return the measurement
+            climberInABox::getMeasurement,
+            // This should return the setpoint (can also be a constant)
+            Constants.WINCH_SPEED,
+            // This uses the output
+            output -> {
+                // Use the output here
+            });
 
         climberInABox = subsystemClimb;
 
@@ -20,9 +36,7 @@ public class ClimbForward extends CommandBase {
 
     @Override
     public void initialize() {
-
         climberInABox.runClimberForward();
-
     }
     
     @Override
