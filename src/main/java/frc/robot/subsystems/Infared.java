e6203427caab076b8ae9272e7f1b47d4deab8da8
fluffhead921 <routhmaya@gmail.com>
2022-02-18 16:13:56 -0600

    Copied and changed code
    Took code from the rev Robotics example for their infrared sensor
    Took code from our color sensor code in the shooter class of the Intake and Shooter branch.
    Mashed them both together and fixes errors.
    
    --Braden

--- src/main/java/frc/robot/subsystems/Infared.java
+++ src/main/java/frc/robot/subsystems/Infared.java
@@ -1,31 +1,40 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.I2C;
 
 import com.revrobotics.*;
+import com.revrobotics.Rev2mDistanceSensor;
 import com.revrobotics.Rev2mDistanceSensor.Port;
 
 public class Infared {
     
     /* Variables */
-    private Rev2mDistanceSensor distOnboard;
     private Rev2mDistanceSensor distMXP;
+
+    // Putting I2C before Port here seems to break stuff, although it may be needed.
+    private Port infraredSensorPort;
+    private Rev2mDistanceSensor infraredSensor;
 
     public Infared() {
 
-        colorSensorPort = I2C.Port.kOnboard;
-
-        colorSensor = new ColorSensorV3(colorSensorPort);
+        // Putting I2C before Port here seems to break stuff, although it may be needed.
+        infraredSensorPort = Port.kOnboard;
+        /* In the rev docs for ir sensors, this variable is labeled distOnboard */
+        infraredSensor = new Rev2mDistanceSensor(infraredSensorPort);
 
         /* TODO: This needs to be put in a better spot, currently it's in a placeholder spot */
         /**
         * Rev 2m distance sensor can be initialized with the Onboard I2C port
         * or the MXP port. Both can run simultaneously.
         */
-        distOnboard = new Rev2mDistanceSensor(Port.kOnboard);
         distMXP = new Rev2mDistanceSensor(Port.kMXP);
 
-        getRange();
+        
+        /* 
+         * This can be used for getting the range of the sensor. 
+         * getRange(); 
+         * */
 
 
         /* TODO: This needs to be put in a better spot, currently it's in a placeholder spot */
@@ -34,7 +43,7 @@ public class Infared {
         * must be called. This starts a background thread which will periodically
         * poll all enabled sensors and store their measured range.
         */
-        distOnboard.setAutomaticMode(true);
+        infraredSensor.setAutomaticMode(true);
 
         
 
