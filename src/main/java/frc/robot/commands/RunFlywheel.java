da472685b201394cb4550f6066dd6e462b149aa7
notaroboticsenthusiast <91231142+notaroboticsenthusiast@users.noreply.github.com>
2022-03-01 16:07:00 -0600

    remove unused imports
    --Gabe

--- src/main/java/frc/robot/commands/RunFlywheel.java
+++ src/main/java/frc/robot/commands/RunFlywheel.java
@@ -4,9 +4,7 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.PIDCommand;
 import frc.robot.Constants;
 import frc.robot.subsystems.Shooter;
 
