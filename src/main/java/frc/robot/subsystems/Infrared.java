1ce3f32017317f58b000adbb33bfdfc9ac2be99e
fluffhead921 <routhmaya@gmail.com>
2022-03-04 17:01:13 -0600

    Removed the second infrared sensor
    (Ignoring color sensor)
    
    --Braden

--- src/main/java/frc/robot/subsystems/Infrared.java
+++ src/main/java/frc/robot/subsystems/Infrared.java
@@ -9,34 +9,28 @@ import com.revrobotics.Rev2mDistanceSensor.Port;
 public class Infrared {
 
     /* Variables */
-    private Rev2mDistanceSensor infraredSensor0;
-    private Rev2mDistanceSensor infraredSensor1;
+    private Rev2mDistanceSensor infraredSensor;
 
-    private int infraredSensorPort0;
-    private int infraredSensorPort1;
+    private int infraredSensorPort;
     
     private I2C multiplexer;
 
     public Infrared() {
 
-        /* TODO: These ports are placeholders, the range is 0-7 */
-        infraredSensorPort0 = 4;
-        infraredSensorPort1 = 2;
+        /* TODO: This port is a placeholder, the range is 0-7 */
+        infraredSensorPort = 4;
 
         multiplexer = new I2C(I2C.Port.kOnboard, 0x70);
 
-        multiplexer.write(0x70, 1 << infraredSensorPort0);
-        infraredSensor0 = new Rev2mDistanceSensor(Port.kOnboard);
-        multiplexer.write(0x70, 1 << infraredSensorPort1);
-        infraredSensor1 = new Rev2mDistanceSensor(Port.kOnboard);
+        multiplexer.write(0x70, 1 << infraredSensorPort);
+        infraredSensor = new Rev2mDistanceSensor(Port.kOnboard);
 
         /**
         * Before measurements can be read from the sensor, setAutomaticMode(true)
         * must be called. This starts a background thread which will periodically
         * poll all enabled sensors and store their measured range.
         */
-        infraredSensor0.setAutomaticMode(true);
-        infraredSensor1.setAutomaticMode(true);
+        infraredSensor.setAutomaticMode(true);
 
     }
 
@@ -44,25 +38,16 @@ public class Infrared {
 
         /* TODO: change the distance at which the sensor detects a ball, it's currently a placeholder */
         /* Gets the range of the sensors and stores them in a variable */
-        multiplexer.write(0x70, 1 << infraredSensorPort0);
-        double distance0 = infraredSensor0.getRange();
-        multiplexer.write(0x70, 1 << infraredSensorPort1);
-        double distance1 = infraredSensor1.getRange();
+        multiplexer.write(0x70, 1 << infraredSensorPort);
+        double distance = infraredSensor.getRange();
 
         /* TODO: These names (infrared0/infrared1 and Present/Missing) suck. Change them */
         /* Lets the driver know via SmartDashboard if there is a ball in a specific spot */
-        if (distance0 <= 1) {
-            SmartDashboard.putString("infrared0", "Present");
+        if (distance <= 1) {
+            SmartDashboard.putString("infraredUndefined", "Present");
         }
         else {
-            SmartDashboard.putString("infrared0", "Missing");
-        }
-    
-        if (distance1 <= 1) {
-            SmartDashboard.putString("infrared1", "Present");
-        }
-        else {
-            SmartDashboard.putString("infrared1", "Missing");
+            SmartDashboard.putString("infraredUndefined", "Missing");
         }
 
     }
