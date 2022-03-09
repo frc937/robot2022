013bedc5de4115535076c75a33bb4da6d35128c9
fluffhead921 <routhmaya@gmail.com>
2022-03-08 18:45:41 -0600

    ACTUALLY refactored Climber.java
    Additional work is required on the commands, but the day is ending.
    
    --Braden

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -6,70 +6,71 @@ import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
+
 import frc.robot.Constants;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-public class Climber extends PIDSubsystem {
+public class Climber extends SubsystemBase {
 
     /* Variables */
     private CANSparkMax winch0;
     private CANSparkMax winch1;
     private double winchSpeed;
     
-    private Encoder winchEncoder;
+    private CANEncoder winchEncoder;
 
-    private SimpleMotorFeedforward winchFeedForward;
+    private CANPIDController winchPID;
     
     /* Creates a new Climber Subsystem */
     public Climber() {
 
-        super(new PIDController(Constants.kWINCH_P, Constants.kWINCH_I, Constants.kWINCH_D));  
-
         /* Initializes winch motor */
-        winch0 = new CANSparkMax(Constants.ID_SPARKMAX_WINCH_0, MotorType.kBrushed);
-        winch1 = new CANSparkMax(Constants.ID_SPARKMAX_WINCH_1, MotorType.kBrushed);
+        winchEncoder = winch0.getEncoder();
 
-        winchEncoder = new Encoder(Constants.DIO_PIN_WINCH_ENCODER_0, Constants.DIO_PIN_WINCH_ENCODER_1);
+        winchPID = winch0.getPIDController();
 
-        winchFeedForward = new SimpleMotorFeedforward(Constants.WINCH_kSVOLTS, Constants.WINCH_kVVOLT_SECONDS_PER_ROTATION);
+        /* This may need to set more constants, like setIZone(), setFF(), or setOutputRange(). idk for now. */
+        winchPID.setP(Constants.kWINCH_P);
+        winchPID.setI(Constants.kWINCH_I);
+        winchPID.setD(Constants.kWINCH_D);
+        winch0.burnFlash();
 
     }
 
-    /**
-     * Sets the winch speed. Takes an output and a setpoint cause PID.
-     * 
-     * <p> These params should be set from the values in {@link frc.robot.Constants}
-     * @param output The output used by PID
-     * @param setpoint The PID setpoint
-     */
-    @Override
-    public void useOutput(double output, double setpoint) {
-        winch0.setVoltage(output + winchFeedForward.calculate(setpoint));
-    }
-
-    @Override
-    public double getMeasurement() {
-        return winchEncoder.getRate();
+    public void setVelocity(double velocity) {
+        winchPID.setReference(velocity, ControlType.kVelocity);
     }
 
     public void runClimberForward() {
-        setSetpoint(Constants.WINCH_SPEED);
+        winch0.set(Constants.WINCH_SPEED);
         winch1.follow(winch0);
     }
 
     public void stopClimber() {
-        setSetpoint(0);
+        winch0.set(0);
         winch1.follow(winch0);
     }
 
     public void reset() {
 
         winchSpeed = -0.30;
-        setSetpoint(winchSpeed);
+        winch0.set(winchSpeed);
         winch1.follow(winch0);
 
+    }
+
+    /**
+     * Safety method to bring ALL motors controlled by this subsystem to a complete stop.
+     */
+    public void stopAll() {
+        winch0.set(0);
+        winch1.follow(winch0);
     }
 
 }
