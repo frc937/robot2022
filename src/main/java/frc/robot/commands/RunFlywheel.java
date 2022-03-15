73c26f3baf70d3313fa06f1464903e33eb414d87
fluffhead921 <routhmaya@gmail.com>
2022-03-15 12:08:37 -0500

    removed unused inport ---Maya

--- src/main/java/frc/robot/commands/RunFlywheel.java
+++ src/main/java/frc/robot/commands/RunFlywheel.java
@@ -7,7 +7,6 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.Flywheel;
-import frc.robot.subsystems.Shooter;
 
 /* This is very intentionally NOT inlined, because we may want to do power optimizations later. */
 /**
