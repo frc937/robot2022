a52e2e1cba4f7a01f580b8d20ee525f79903c209
fluffhead921 <routhmaya@gmail.com>
2022-03-15 10:40:39 -0500

    removed references to flywheel --Maya

--- src/main/java/frc/robot/subsystems/IndexWheel.java
+++ src/main/java/frc/robot/subsystems/IndexWheel.java
@@ -1,47 +1,17 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.SparkMaxPIDController;
-import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class IndexWheel extends SubsystemBase {
 
-    private CANSparkMax flywheel;
     private CANSparkMax controlWheel;
-
-    /*private RelativeEncoder flywheelEncoder;*/
-
-    private SparkMaxPIDController flywheelPID;
 
     /** Creates a new Index Wheel. */
     public IndexWheel() {
-        flywheel = new CANSparkMax(Constants.ID_SPARKMAX_FLYWHEEL, MotorType.kBrushed);
         controlWheel = new CANSparkMax(Constants.ID_SPARKMAX_CONTROL_WHEEL, MotorType.kBrushed);
-
-        /*flywheelEncoder = flywheel.getEncoder();*/
-
-        flywheelPID = flywheel.getPIDController();
-
-        /* This may need to set more constants, like setIZone(), setFF(), or setOutputRange(). idk for now. */
-        flywheelPID.setP(Constants.kFLYWHEEL_P);
-        flywheelPID.setI(Constants.kFLYWHEEL_I);
-        flywheelPID.setD(Constants.kFLYWHEEL_D);
-        flywheel.burnFlash();
-    }
-
-    public void setVelocity(double velocity) {
-        flywheelPID.setReference(velocity, ControlType.kVelocity);
-    }
-
-    public void testFlywheel() {
-        flywheel.set(0.7);
-    }
-
-    public void stopFlywheel() {
-        flywheel.set(0);
     }
 
     /**
@@ -55,14 +25,6 @@ public class IndexWheel extends SubsystemBase {
      * Stops the feeder motor.
      */
     public void stopFeeder() {
-        controlWheel.set(0);
-    }
-
-    /**
-     * Safety method to bring ALL motors controlled by this subsystem to a complete stop.
-     */
-    public void stopAll() {
-        flywheel.set(0);
         controlWheel.set(0);
     }
 
