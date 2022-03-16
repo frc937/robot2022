e54eaa730a1aff8a00ca40033d465924f4470fe6
notaroboticsenthusiast <91231142+notaroboticsenthusiast@users.noreply.github.com>
2022-03-16 13:09:32 -0500

    turn off Limelight LEDs by default
    --Gabe

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -18,6 +18,8 @@ public class Limelight extends SubsystemBase {
     /** Creates a new Limelight. */
     public Limelight() {
         this.limelightTable = NetworkTableInstance.getDefault().getTable("limelight");
+        /* Turn off the Limelight's LEDs by default. Commands that use it will turn them on. */
+        limelightTable.getEntry("ledMode").setNumber(1.0);
     }
 
     /**
