639765072a757289f8036c444c939d69148922cc
fluffhead921 <routhmaya@gmail.com>
2022-03-21 17:47:35 -0500

    Literally the same thing as earlier
    --Braden

---
+++ src/main/java/frc/robot/commands/ClimbDown.java
@@ -0,0 +1,38 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.Climber;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class ClimberDown extends CommandBase {
+
+    /* Variables */
+    private Climber climberInABox;
+
+    /* Initializes the ClimbUp method */
+    public ClimberDown(Climber subsystemClimb) {
+
+        climberInABox = subsystemClimb;
+
+        addRequirements(subsystemClimb);
+
+    }
+
+    @Override
+    public void initialize() {
+
+        climberInABox.runClimberDown();
+
+    }
+    
+    @Override
+    public void end(boolean interrupted) {
+        climberInABox.stopClimber();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+}
