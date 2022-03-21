7546195f61289fb05fddd580e3bc17968f72ca73
fluffhead921 <routhmaya@gmail.com>
2022-03-21 16:46:34 -0500

    Removed/Changed PID stuff to motor stuff
    --Braden

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -2,15 +2,13 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj2.command.PIDSubsystem;
-import edu.wpi.first.math.controller.PIDController;
 
 import frc.robot.Constants;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-public class Climber extends PIDSubsystem {
+public class Climber extends SubsystemBase {
 
     /* Variables */
     private CANSparkMax winch0;
@@ -22,8 +20,6 @@ public class Climber extends PIDSubsystem {
     /* Creates a new Climber Subsystem */
     public Climber() {
 
-        super(new PIDController(Constants.kWINCH_P, Constants.kWINCH_I, Constants.kWINCH_D));
-
         /* Initializes winch motor */
         winch0 = new CANSparkMax(Constants.ID_SPARKMAX_WINCH_0, MotorType.kBrushed);
         winch1 = new CANSparkMax(Constants.ID_SPARKMAX_WINCH_1, MotorType.kBrushed);
@@ -34,28 +30,29 @@ public class Climber extends PIDSubsystem {
 
     }
 
-    @Override
-    public void useOutput(double output, double setpoint) {
-        winch0.setVoltage(output + setpoint);
-    }
+    /* I think this method might be useless now without PID */
+    // @Override
+    // public void useOutput() {
+    //     winch0.set(0);
+    // }
 
-    @Override
-    public double getMeasurement() {
-        return winchEncoder.getRate();
-    }
+    // @Override
+    // public double getMeasurement() {
+    // return winchEncoder.getRate();
+    // }
 
     public void runClimberForward() {
-        setSetpoint(Constants.WINCH_SPEED);
+        winch0.set(Constants.WINCH_SPEED);
     }
 
     public void stopClimber() {
-        setSetpoint(0);
+        winch0.set(0);
     }
 
     public void reset() {
 
         winchSpeed = -0.30;
-        setSetpoint(winchSpeed);
+        winch0.set(winchSpeed);
 
     }
 
