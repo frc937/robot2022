0a1b8df6fbf94a49c6bf300ef65b4aae936295b1
Jack-Haefele <113944276+Jack-Haefele@users.noreply.github.com>
2024-02-21 18:30:41 -0600

    removed unused imports

--- src/main/java/frc/robot/subsystems/IndexWheel.java
+++ src/main/java/frc/robot/subsystems/IndexWheel.java
@@ -1,8 +1,5 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
